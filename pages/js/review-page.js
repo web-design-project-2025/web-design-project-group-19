7a03c7eedb3bd692311ce82a043c570e2b8dc762
aminaism <aaminamohamedf@gmail.com>
2025-05-13 20:06:22 +0200

    review page js

--- pages/js/review-page.js
+++ pages/js/review-page.js
@@ -1,2 +1,14 @@
-import { Movies, TV, getImageUrl } from "./api-tmdb.js";
+import { Movies, getImageUrl } from "./api-tmdb.js";
+
+// Function to display movie details
+async function loadMovieDetails(movieId) {
+    try {
+        const movie = await Movies.getDetails(movieId);
+        document.querySelector(".movie-poster").src = getImageUrl(movie.poster_path);
+        document.querySelector("movie-title").textContent = movie.title;
+        document.querySelector("movie-overview").textContent = movie.overview;
+    } catch (error) {
+        consoler.error("Failed to load movie details:", error);
+    }
+}
 
