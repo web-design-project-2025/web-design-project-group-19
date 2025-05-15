04875b807ac7d500ba41bdc23705d6e69dcdcd51
sopphiaaa <olso24gz@student.ju.se>
2025-05-15 23:55:58 +0200

    add of functionalities

---
+++ pages/js/firebase-config.js
@@ -0,0 +1,24 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCaCok0sNM7U97CDFGnV1fpdR8DaK2kq3M",
+  authDomain: "seenit-9ec06.firebaseapp.com",
+  projectId: "seenit-9ec06",
+  storageBucket: "seenit-9ec06.firebasestorage.app",
+  messagingSenderId: "437565688701",
+  appId: "1:437565688701:web:8ca099d80bef9e63a29a14",
+};
+
+// Initialize Firebase
+if (typeof firebase !== "undefined" && !firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+// Initialize services
+const db = firebase.firestore(); // Now this should work
+const auth = firebase.auth();
+
+// Make available globally if needed
+window.db = db;
+window.auth = auth;
+
+//Firebase used on suggestion of Janos Papp - Sophia's friend
